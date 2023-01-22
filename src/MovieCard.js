a777d3c126964a638c99b4653afa286fc2788eeb
Nemanja43M <nemanjaamarkovic91@gmail.com>
2023-01-23 00:55:14 +0100

    axios and useState funcionality

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,14 +1,6 @@
 import React from "react";
 
-function MovieCard() {
-  const movie = {
-    Title: "Iron Man",
-    Year: "2008",
-    imdbID: "tt0371746",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg",
-  };
+function MovieCard({ movie }) {
   return (
     <div className="col-md-4">
       <img src={movie.Poster} alt="Poster" />
