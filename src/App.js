5fb7cae73c985730caba3e23ff2fdbdf6e460358
Nemanja43M <nemanjaamarkovic91@gmail.com>
2023-01-23 14:38:49 +0100

    Routing SPA

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import Header from "./Header";
-// import Home from "./Home";
+import Home from "./Home";
 import style from "./App.module.css";
 import MovieList from "./MovieList";
 // import MovieCard from "./MovieCard";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className={style.App}>
-      <Header />
-      {/* <Home />
-      <MovieCard /> */}
-      <div className="container">
-        <MovieList />
+    <BrowserRouter>
+      <div className={style.App}>
+        <Header />
+        <Route path="/" exact={true} component={Home}></Route>
+        <div className="container">
+          <Route path="/movies" component={MovieList}></Route>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/MovieList.js
+++ src/MovieList.js
@@ -2,14 +2,18 @@ import React from "react";
 import MovieCard from "./MovieCard";
 import axios from "axios";
 import { useState } from "react";
+import queryString from "query-string";
 
-function MovieList() {
+function MovieList(props) {
   const [movie, setMovie] = useState([]);
 
   function fetchMovie() {
-    let search = "thor";
+    let q = queryString.parse(props.location.search);
+    console.log(q);
     axios
-      .get(`https://www.omdbapi.com/?i=tt3896198&apikey=28101efb&s=${search}`)
+      .get(
+        `https://www.omdbapi.com/?i=tt3896198&apikey=28101efb&s=${q.searchText}`
+      )
       .then((resp) => resp.data)
       .then((data) => data.Search)
       .then((movies) => setMovie(movies));
